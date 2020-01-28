fea493b3c24c3a16633734182cc7e8d2f2ee5071
JungHyoMoon <dsinhome70@gmail.com>
2020-01-28 14:55:42 +0900

    gulp setup (autoprefixer and csso)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,9 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+
+sass.compiler = require("node-sass");
 
 const paths = {
   styles: {
@@ -12,5 +16,12 @@ export function styles() {
   return gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
 }
