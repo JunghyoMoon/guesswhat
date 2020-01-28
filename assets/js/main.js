93da3038fea65afce27074fd9f5d5be728a54edb
JungHyoMoon <dsinhome70@gmail.com>
2020-01-28 15:40:51 +0900

    working on browserify setup (we need some configurations to solve error)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import { handleMessageNotif } from "./chat";
+
 const socket = io("/");
 
 function sendMessage(message) {
@@ -8,11 +10,6 @@ function sendMessage(message) {
 function setNickName(nickName) {
   socket.emit("setNickName", { nickName });
   console.log(`Your nickname is ${nickName}`);
-}
-
-function handleMessageNotif(data) {
-  const { message, nickName } = data;
-  console.log(`${nickName}: ${message}`);
 }
 
 socket.on("messageNotif", handleMessageNotif);
