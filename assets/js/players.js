5c11e2d60d456701cb16fb9792d2e3a0dc9e6702
JungHyoMoon <dsinhome70@gmail.com>
2020-02-20 15:02:02 +0900

    working on basic game logic

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,1 +1,12 @@
-export const handlePlayerUpdate = ({ sockets }) => console.log(sockets);
+const board = document.getElementById("jspBoard");
+
+const addPlayers = players => {
+  board.innerHTML = "";
+  players.forEach(player => {
+    const playerElement = document.createElement("span");
+    playerElement.innerText = `${player.nickName}: ${player.point}`;
+    board.appendChild(playerElement);
+  });
+};
+
+export const handlePlayerUpdate = ({ sockets }) => addPlayers(sockets);
